4230726f417404aa03aaff9d4c064bfce00e0a35
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2017-10-16 10:09:54 +1300

    google map api integration.

--- app/src/main/java/au/com/miacucina/com/foodtour/MainActivity.java
+++ app/src/main/java/au/com/miacucina/com/foodtour/MainActivity.java
@@ -1,5 +1,6 @@
 package au.com.miacucina.com.foodtour;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -82,6 +83,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            Intent tourMapIntent = new Intent(this, TourMapsActivity.class);
+            this.startActivity(tourMapIntent);
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
